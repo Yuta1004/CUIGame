3c4f708623056f568a2ccffc463b4ebad39008d5
NakagamiYuta <guguru0014@gmail.com>
2019-11-02 02:04:25 +0900

    [change] GameManagerのコンストラクタ, デストラクタを変更

--- gamemanager.cpp
+++ gamemanager.cpp
@@ -1,11 +1,17 @@
 #include "gamemanager.h"
 #include "game.h"
+#include "window.h"
 
 using namespace mgameman;
 
-GameManager::GameManager(mgame::Game *game) {
+GameManager::GameManager(mgame::Game *game, int width, int height) {
     this->game = game;
     this->frameRate = 10;
+    this->win = new cuiwin::Window(width, height);
+}
+
+GameManager::~GameManager() {
+    free(this->win);
 }
 
 void GameManager::setFrameRate(int frameRate) {
--- gamemanager.h
+++ gamemanager.h
@@ -2,18 +2,21 @@
 #define GAMEMANAGER
 
 #include "game.h"
+#include "window.h"
 
 namespace mgameman {
 
     class GameManager {
 
     public:
-        GameManager(mgame::Game *game);
+        GameManager(mgame::Game *game, int width, int height);
+        ~GameManager();
         void setFrameRate(int frameRate);
         void run();
 
     private:
         mgame::Game *game;
+        cuiwin::Window *win;
         int frameRate;
     };
 }
