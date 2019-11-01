de127cfc0ba869ce94f29b539879c473c4a687ee
NakagamiYuta <guguru0014@gmail.com>
2019-11-02 00:07:14 +0900

    [add] 盤面の区切り線ほかコメント追加

--- main.cpp
+++ main.cpp
@@ -6,10 +6,19 @@
 #define MIC 1
 
 int main() {
+    // Window初期化
     cuiwin::Window win(50, 40);
+    win.setChar('*');
 
+    // メインループ
     while(1) {
         win.refresh();
+
+        // 盤面の区切り
+        win.setChar('|');
+        win.drawRect(30, 0, 1, 40);
+
+        // 画面更新
         win.update();
         usleep(1*SEC);
     }
