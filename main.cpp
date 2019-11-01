5e062843e2e4c3224025fcd88d666fe49def9e08
NakagamiYuta <guguru0014@gmail.com>
2019-11-02 00:14:43 +0900

    [add] score, line, level情報を描画するようにした

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@
 #define MIC 1
 
 int main() {
+    int score = 0, line = 0, level = 1;
+    char *s = (char*)malloc(15);
+
     // Window初期化
-    cuiwin::Window win(50, 40);
+    cuiwin::Window win(55, 40);
     win.setChar('*');
 
     // メインループ
@@ -18,8 +21,19 @@ int main() {
         win.setChar('|');
         win.drawRect(30, 0, 1, 40);
 
+        // 各種情報
+        sprintf(s, "SCORE: %d", score);
+        win.drawText(33, 5, s);
+        sprintf(s, "LINE : %d", line);
+        win.drawText(33, 7, s);
+        sprintf(s, "LEVEL: %d", level);
+        win.drawText(33, 9, s);
+
         // 画面更新
         win.update();
         usleep(1*SEC);
     }
+
+    // 後処理
+    free(s);
 }
