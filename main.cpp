4e873977c16b23b429c2f9554939be933d6ce0ab
NakagamiYuta <guguru0014@gmail.com>
2019-11-03 15:07:54 +0900

    [add] main実行時に起動するゲームを選べるようにした

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
+#include <string>
+#include <iostream>
 #include "game/game.h"
 #include "tetris/tetris.h"
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+yngame::Game* startupGame(int gameID) {
+    switch(gameID) {
+    case 1:
+        return (yngame::Game*)new TETRIS();
+    default:
+        return (yngame::Game*)new TETRIS();;
+    }
+}
+
 int main() {
-    yngame::Game *tetris = (yngame::Game*)new TETRIS();
-    tetris->setFrameRate(30);
-    tetris->run();
+    std::ios::sync_with_stdio();
+
+    int gameID;
+    cout << "----- Select Game ----" << endl;
+    cout << "1. TETRIS" << endl;
+    cout << "\n>> ";
+    cin >> gameID;
+    cout << "\n\n" << endl;;
+
+    yngame::Game *game = startupGame(gameID);
+    game->setFrameRate(30);
+    game->run();
+    free(game);
 }
 
